70e76000ebadccec8ecba09ac4c41824be47d8a0
Thiago Antonello Vargas <thiantonello@gmail.com>
2022-01-20 12:31:57 -0300

    test: exclude private method test and refactor

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,10 +1,12 @@
 require './lib/parser'
 require './spec/spec_helper'
 
-describe Parser do  
+describe Parser do
+  let(:path) { "./spec/fixtures/games_test.log" }
+
   describe "#first_line" do
     context "when the file exist" do
-      subject { described_class.new("./spec/fixtures/games_test.log") }
+      subject { described_class.new(path) }
       
       it "return the first line of the file" do
         expect(subject.first_line).to eq("  0:00 ------------------------------------------------------------\n")
@@ -18,31 +20,18 @@ describe Parser do
         expect { subject.first_line }.to raise_error(Errno::ENOENT)
       end
     end
-  end
-
-  describe "#count_lines" do
-    subject { described_class.new("./spec/fixtures/games_test.log") }
-
-    it "returns the number of lines in the file" do
-      expect(subject.count_lines).to eq(158)
-    end
-
-    #verificar com os avaliadores/reviwers qual modo usar para testar o count_lines (de cima ou de baixo)
-
-    it { expect(subject.count_lines).to eq(158)}
-  end
+  end  
 
   describe "#count_lines_json" do
-    subject { described_class.new("./spec/fixtures/games_test.log") }
+    subject { described_class.new(path).count_lines_json }
 
-    let(:obj) { { "games_test.log" => {
-      "lines" => 158
-    } } }
-    
-    specify { expect(JSON.parse(subject.count_lines_json)).to eq(obj) }
-    
-    #verificar com os avaliadores/reviewers qual modo usar para testar o count_lines_json (de cima ou de baixo)
-    
-    it { expect(subject.count_lines_json).to eq(JSON.pretty_generate(obj)) }
+    let(:obj) do      
+      { "games_test.log" => { 
+        "lines" => 158
+        }
+      }
+    end
+        
+    it { is_expected.to eq(JSON.pretty_generate(obj)) }
   end
 end
